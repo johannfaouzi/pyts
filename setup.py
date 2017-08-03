514f1115321d68a0df84be0f2d9f7a418dd795c3
Johann Faouzi <joh.fao@orange.fr>
2017-08-03 21:17:19 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='pyts',
+    version='0.4',
+    description='A package for transformation and classification of time series',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+      ],
+    keywords='time series machine learning transformation classification',
+    url='https://github.com/johannfaouzi/pyts',
+    author='Johann Faouzi',
+    author_email='johann.faouzi@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'sklearn',
+        'matplotlib'
+    ],
+    zip_safe=False)
