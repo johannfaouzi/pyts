ce205328bd214e8a9f2a867eaf911427ae2e02c7
johann.faouzi <johann.faouzi@icm-institute.org>
2018-05-18 12:14:15 +0200

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='pyts',
     description='A package for transformation and classification of time series',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD-3 License',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
