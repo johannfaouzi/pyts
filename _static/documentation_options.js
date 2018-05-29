d6d20526d52d6ef7b500a295b6bfc0b3815e02dd
johannfaouzi <johann.faouzi@gmail.com>
2018-05-29 07:41:11 +0000

    Pushing the docs for revision for branch: master, commit 0ea3dd26286ac817b26870e4348a1eef986c0007

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.7.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
