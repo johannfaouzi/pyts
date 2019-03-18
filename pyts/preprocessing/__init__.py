5e369c30ee0016de23e1c6fab3295c2a58889170
johann.faouzi <johann.faouzi@icm-institute.org>
2019-03-18 17:53:23 +0100

    Add InterpolationImputer in __init__.py

--- pyts/preprocessing/__init__.py
+++ pyts/preprocessing/__init__.py
@@ -3,7 +3,14 @@
 from .scaler import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
 from .transformer import PowerTransformer, QuantileTransformer
 from .discretizer import KBinsDiscretizer
+from .imputer import InterpolationImputer
 
 
-__all__ = ['StandardScaler', 'MinMaxScaler', 'MaxAbsScaler', 'RobustScaler',
-           'PowerTransformer', 'QuantileTransformer', 'KBinsDiscretizer']
+__all__ = ['StandardScaler',
+           'MinMaxScaler',
+           'MaxAbsScaler',
+           'RobustScaler',
+           'PowerTransformer',
+           'QuantileTransformer',
+           'KBinsDiscretizer',
+           'InterpolationImputer']
