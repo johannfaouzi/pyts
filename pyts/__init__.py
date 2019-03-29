74b1f463483e0ec770900b1b7eca4d79e7b3dea1
johann.faouzi <johann.faouzi@icm-institute.org>
2019-03-29 09:56:26 +0100

    Fix typos in __init__.py

--- pyts/__init__.py
+++ pyts/__init__.py
@@ -1,13 +1,13 @@
 """Time series transformation and classification module for Python.
 
-pyts is a Python module integrating classical and recently published
-algorithms used for time series transformation and classification.
-
-It aims to provide implementations for state-of-the-art as well as
-recently published algorithms for time series classification.
+pyts is a Python package for time series transformation and classification.
+It aims to make time series classification easily accessible by providing
+preprocessing and utility tools, and implementations of state-of-the-art
+algorithms. Most of these algorithms transform time series, thus pyts provides
+several tools to perform these transformations.
 """
 
 __version__ = '0.8.0'
 
-__all__ = ['approximation', 'bag_of_words', 'classification', 'image',
-           'preprocessing', 'transformation', 'utils']
+__all__ = ['approximation', 'bag_of_words', 'classification', 'decomposition',
+           'image', 'metrics', 'preprocessing', 'transformation', 'utils']
