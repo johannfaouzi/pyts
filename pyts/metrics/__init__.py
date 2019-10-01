a8feced085b68c3f98b8dc446d258735ce140c59
Johann Faouzi <johann.faouzi@gmail.com>
2019-10-01 12:15:26 +0200

    Add lower bounds in init file

--- pyts/metrics/__init__.py
+++ pyts/metrics/__init__.py
@@ -4,7 +4,12 @@ from .boss import boss
 from .dtw import (dtw_classic, dtw_region, dtw_sakoechiba, dtw_itakura,
                   dtw_multiscale, dtw_fast, dtw, itakura_parallelogram,
                   sakoe_chiba_band, show_options)
+from .lower_bounds import (lower_bound_improved, lower_bound_keogh,
+                           lower_bound_kim, lower_bound_yi)
+
 
 __all__ = ['boss', 'dtw_classic', 'dtw_region', 'dtw_sakoechiba',
            'dtw_itakura', 'dtw_multiscale', 'dtw_fast', 'dtw',
-           'itakura_parallelogram', 'sakoe_chiba_band', 'show_options']
+           'itakura_parallelogram', 'lower_bound_improved',
+           'lower_bound_keogh', 'lower_bound_kim', 'lower_bound_yi',
+           'sakoe_chiba_band', 'show_options']
