49a15a6612251c77d4161a6a24ac714a0f619fc1
Johann Faouzi <johann.faouzi@gmail.com>
2019-11-27 17:15:08 +0100

    Remove show_options from __init__

--- pyts/metrics/__init__.py
+++ pyts/metrics/__init__.py
@@ -3,7 +3,7 @@
 from .boss import boss
 from .dtw import (dtw_classic, dtw_region, dtw_sakoechiba, dtw_itakura,
                   dtw_multiscale, dtw_fast, dtw, itakura_parallelogram,
-                  sakoe_chiba_band, show_options)
+                  sakoe_chiba_band)
 from .lower_bounds import (lower_bound_improved, lower_bound_keogh,
                            lower_bound_kim, lower_bound_yi)
 
@@ -12,4 +12,4 @@ __all__ = ['boss', 'dtw_classic', 'dtw_region', 'dtw_sakoechiba',
            'dtw_itakura', 'dtw_multiscale', 'dtw_fast', 'dtw',
            'itakura_parallelogram', 'lower_bound_improved',
            'lower_bound_keogh', 'lower_bound_kim', 'lower_bound_yi',
-           'sakoe_chiba_band', 'show_options']
+           'sakoe_chiba_band']
