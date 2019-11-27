6452c6649978430a58b77996a9629dad3af6d9d9
Johann Faouzi <johann.faouzi@gmail.com>
2019-11-27 11:56:31 +0100

    Add ShapeletTransform in init

--- pyts/transformation/__init__.py
+++ pyts/transformation/__init__.py
@@ -1,6 +1,7 @@
 """The :mod:`pyts.transformation` module includes transformation algorithms."""
 
 from .boss import BOSS
+from .shapelet_transform import ShapeletTransform
 from .weasel import WEASEL
 
-__all__ = ['BOSS', 'WEASEL']
+__all__ = ['BOSS', 'ShapeletTransform', 'WEASEL']
