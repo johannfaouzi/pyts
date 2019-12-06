49d8198357714ec423043225ed554d0d6e6428e3
Johann Faouzi <johann.faouzi@gmail.com>
2019-12-06 11:36:04 +0100

    Add python3.8 in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ CLASSIFIERS = ['Development Status :: 3 - Alpha',
                'Operating System :: MacOS',
                'Programming Language :: Python :: 3.5',
                'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8']
 EXTRAS_REQUIRE = {
     'tests': [
         'pytest',
