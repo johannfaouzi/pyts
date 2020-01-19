b58693f676b22a1b1dc6bf7ff0ad1b3b690264f7
Johann Faouzi <johann.faouzi@gmail.com>
2020-01-19 14:14:05 +0100

    Add LearningShapelets classifier

--- pyts/classification/__init__.py
+++ pyts/classification/__init__.py
@@ -1,7 +1,8 @@
 """The :mod:`pyts.classification` module includes classification algorithms."""
 
 from .bossvs import BOSSVS
+from .learning_shapelets import LearningShapelets
 from .knn import KNeighborsClassifier
 from .saxvsm import SAXVSM
 
-__all__ = ['BOSSVS', 'KNeighborsClassifier', 'SAXVSM']
+__all__ = ['BOSSVS', 'KNeighborsClassifier', 'LearningShapelets', 'SAXVSM']
