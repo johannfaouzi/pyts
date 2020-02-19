dbf415697394f8b179620dc573332d4704e32c0f
Johann Faouzi <johann.faouzi@gmail.com>
2020-02-19 13:12:41 +0100

    Readd previous BagOfWords as WordExtractor (#64)

--- pyts/bag_of_words/tests/test_bow.py
+++ pyts/bag_of_words/tests/test_bow.py
@@ -6,10 +6,68 @@
 import numpy as np
 import pytest
 import re
-from pyts.bag_of_words import BagOfWords
+from pyts.bag_of_words import BagOfWords, WordExtractor
+
+# ######################### Tests for WordExtractor #########################
+
+X_word = [['a', 'a', 'a', 'b', 'a'],
+          ['a', 'a', 'b', 'b', 'a'],
+          ['b', 'b', 'b', 'b', 'a']]
 
 
-X = np.arange(20).reshape(2, 10)
+@pytest.mark.parametrize(
+    'params, error, err_msg',
+    [({'window_size': '4'}, TypeError,
+      "'window_size' must be an integer or a float."),
+     ({'window_step': [0, 1]}, TypeError,
+      "'window_step' must be an integer or a float."),
+     ({'window_size': 0}, ValueError,
+      "If 'window_size' is an integer, it must be greater than or equal to 1 "
+      "and lower than or equal to n_timestamps (got 0)."),
+     ({'window_size': 2.}, ValueError,
+      "If 'window_size' is a float, it must be greater than 0 and lower "
+      "than or equal to 1 (got {0}).".format(2.)),
+     ({'window_step': 0}, ValueError,
+      "If 'window_step' is an integer, it must be greater than or equal to 1 "
+      "and lower than or equal to n_timestamps (got 0)."),
+     ({'window_step': 2.}, ValueError,
+      "If 'window_step' is a float, it must be greater than 0 and lower "
+      "than or equal to 1 (got {0}).".format(2.))]
+)
+def test_parameter_check_word_extractor(params, error, err_msg):
+    """Test parameter validation."""
+    bow = WordExtractor(**params)
+    with pytest.raises(error, match=re.escape(err_msg)):
+        bow.transform(X_word)
+
+
+@pytest.mark.parametrize(
+    'params, arr_desired',
+    [({}, ['a b a', 'a b a', 'b a']),
+     ({'numerosity_reduction': False},
+      ['a a a b a', 'a a b b a', 'b b b b a']),
+     ({'window_size': 1, 'numerosity_reduction': False},
+      ['a a a b a', 'a a b b a', 'b b b b a']),
+     ({'window_size': 2, 'numerosity_reduction': False},
+      ['aa aa ab ba', 'aa ab bb ba', 'bb bb bb ba']),
+     ({'window_size': 0.4, 'window_step': 0.2, 'numerosity_reduction': False},
+      ['aa aa ab ba', 'aa ab bb ba', 'bb bb bb ba']),
+     ({'window_size': 2, 'window_step': 1, 'numerosity_reduction': True},
+      ['aa ab ba', 'aa ab bb ba', 'bb ba']),
+     ({'window_size': 3, 'window_step': 2, 'numerosity_reduction': False},
+      ['aaa aba', 'aab bba', 'bbb bba']),
+     ({'window_size': 3, 'window_step': 2, 'numerosity_reduction': True},
+      ['aaa aba', 'aab bba', 'bbb bba'])]
+)
+def test_actual_results_word_extractor(params, arr_desired):
+    """Test that the actual results are the expected ones."""
+    arr_actual = WordExtractor(**params).fit_transform(X_word)
+    np.testing.assert_array_equal(arr_actual, arr_desired)
+
+
+# ######################### Tests for BagOfWords #########################
+
+X_bow = np.arange(20).reshape(2, 10)
 
 
 @pytest.mark.parametrize(
@@ -74,11 +132,11 @@ X = np.arange(20).reshape(2, 10)
        'alphabet': ['a', 'b', 'c']}, ValueError,
       "If 'alphabet' is array-like, its shape must be equal to (n_bins,).")]
 )
-def test_parameter_check(params, error, err_msg):
+def test_parameter_check_bag_of_words(params, error, err_msg):
     """Test parameter validation."""
     bow = BagOfWords(**params)
     with pytest.raises(error, match=re.escape(err_msg)):
-        bow.transform(X)
+        bow.transform(X_bow)
 
 
 @pytest.mark.parametrize(
@@ -108,7 +166,7 @@ def test_parameter_check(params, error, err_msg):
      ({'window_size': 4, 'word_size': 4, 'window_step': 0.5},
       ['abcd', 'abcd'])]
 )
-def test_actual_results(params, arr_desired):
+def test_actual_results_bag_of_words(params, arr_desired):
     """Test that the actual results are the expected ones."""
-    arr_actual = BagOfWords(**params).fit_transform(X)
+    arr_actual = BagOfWords(**params).fit_transform(X_bow)
     np.testing.assert_array_equal(arr_actual, arr_desired)
